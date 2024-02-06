6d83a8bbbbf50de325c5eed6f48ca4498eb624f2
sipt <wxysipt@gmail.com>
2024-02-06 15:21:42 +0800

    add download link

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
-import screenShort from "./assets/SCR-20240204-kc0.png";
+import screenShort from "./assets/Untitled.png";
 import icon from "./assets/icon-white.svg";
 import colorIcon from "./assets/icon-color.svg";
 import twitter from "./assets/Twitter.svg";
 import telegram from "./assets/Telegram.svg";
+import apple from "./assets/apple.svg";
+import windows from "./assets/windows.svg";
 
 function App() {
   return (
@@ -22,9 +24,9 @@ function App() {
           </a>
         </div>
       </div>
-      <div className="w-full flex-grow flex">
-        <div className="w-full md:h-[500px] flex md:flex-row flex-col justify-start md:justify-between md:mx-[100px]">
-          <div className="flex flex-col justify-center items-center md:items-start  my-[64px] md:my-0">
+      <div className="w-full flex-grow flex flex-col mb-10">
+        <div className=" max-w-full flex md:flex-row flex-col justify-start md:justify-between md:mx-[100px]">
+          <div className="flex flex-col justify-center items-center md:items-start  my-[64px] md:my-0 gap-[16px]">
             <div className="mx-5 max-w-[500px] flex flex-col gap-[16px] items-center md:items-start">
               <img src={colorIcon} className="h-[64px] w-[64px]" />
               <p className="md:text-5xl text-2xl font-bold">ChatHarvest</p>
@@ -37,8 +39,33 @@ function App() {
             <img src={screenShort} alt="screen short" />
           </div>
         </div>
+        <div className="flex flex-col md:flex-row gap-10 justify-center items-center flex-grow">
+          <div className="rounded-full bg-theme px-5 py-2 flex flex-row items-center gap-2">
+            <img src={apple} />
+            <span className="font-bold">macOS</span>
+            <a
+              href="https://apps.sipt.top/ChatHarvest-1.0.0-amd64.dmg"
+              className="bg-black/30 hover:bg-black/20 px-3 py-2 rounded-lg cursor-pointer"
+            >
+              Intel
+            </a>
+            <a
+              href="https://apps.sipt.top/ChatHarvest-1.0.0-amd64.dmg"
+              className="bg-black/30 hover:bg-black/20 px-3 py-2 rounded-lg cursor-pointer"
+            >
+              Apple silicon
+            </a>
+          </div>
+          <a
+            href="https://apps.sipt.top/ChatHarvest-1.0.0%20Setup.exe"
+            className="rounded-full bg-theme px-5 py-4 flex flex-row items-center gap-2 w-fit"
+          >
+            <img src={windows} />
+            <span className="font-bold">Windows</span>
+          </a>
+        </div>
       </div>
-      <div className="h-[64px] w-full flex flex-row items-center justify-between px-[16px] md:px-[64px]">
+      <div className="min-h-[64px] w-full flex flex-row items-center justify-between px-[16px] md:px-[64px]">
         <span>©2023 ChatHarvest</span>
         <a href="/privacy">Privacy Policy</a>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // 主题色
+        theme: "#BF5AF2",
+      },
+    },
   },
   plugins: [],
 };
